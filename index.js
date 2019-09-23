16a2963e12fb86e3d1b3e57833480fabbfeb21ce
Chris Troutner <chris.troutner@gmail.com>
2019-09-23 13:36:07 -0700

    First steps in replicated the tx details call

--- index.js
+++ index.js
@@ -7,7 +7,8 @@
 'use strict'
 
 const BCHJS = require('@chris.troutner/bch-js')
-const bchjs = new BCHJS({restURL: `http://192.168.0.36:12400/v3/`})
+//const bchjs = new BCHJS({restURL: `http://192.168.0.36:12400/v3/`})
+const bchjs = new BCHJS({restURL: `http://localhost:3000/v3/`})
 
 const scriptPubKey = require('./lib/get-scriptpubkey')
 
@@ -21,7 +22,7 @@ class BB2Insight {
   async details(cashAddr) {
     // Get raw data from Blockbook.
     const bbData = await bchjs.Blockbook.balance(cashAddr)
-    //console.log(`Blockbook original data: ${JSON.stringify(bbData,null,2)}`)
+    // console.log(`Blockbook original data: ${JSON.stringify(bbData,null,2)}`)
 
     const newData = {}
 
@@ -45,6 +46,7 @@ class BB2Insight {
     newData.transactions = bbData.txids
 
     newData.cashAddress = bbData.address
+    console.log(`newData.cashAddress: ${newData.cashAddress}`)
     newData.legacyAddress = bchjs.Address.toLegacyAddress(newData.cashAddress)
 
     newData.currentPage = bbData.page - 1
@@ -82,6 +84,50 @@ class BB2Insight {
     }
 
     return newData
+  }
+
+  async transactions(cashAddr) {
+    // Get address details
+    const addrDetails = await this.details(cashAddr)
+    // console.log(`addrDetails: ${JSON.stringify(addrDetails,null,2)}`)
+
+    // Scaffold the output object.
+    const outObj = {
+      cashAddress: addrDetails.cashAddress,
+      legacyAddress: addrDetails.legacyAddress,
+      pagesTotal: addrDetails.pagesTotal,
+      curentPage: addrDetails.currentPage
+    }
+
+    // Combine tx details into an array.
+    const txIds = addrDetails.transactions
+
+    // TODO add some code here to break up the txIds array into 20 element chunks.
+    // Need to add test cases for this corner case.
+
+    // Use the bulk method for retrieving details on 20 transactions at a time.
+    const txDetails = await bchjs.RawTransactions.getRawTransaction(txIds, true)
+    console.log(`txDetails: ${JSON.stringify(txDetails,null,2)}`)
+
+    // Loop through each transaction to add metadata like valueIn, valueOut,
+    // and fees.
+    for(let i=0; i < txDetails.length; i++) {
+      const thisTx = txDetails[i]
+
+      // Get the total value output in the transaction (in BCH).
+      let valueOut = 0
+      thisTx.vout.map(vout => valueOut += vout.value)
+      thisTx.valueOut = valueOut
+
+      // TODO: Add valueIn and fees for each tx.
+      // Research the blockbook API first though:
+      // https://github.com/trezor/blockbook/blob/master/docs/api.md#get-transaction
+    }
+
+    // Add the TX details to the output object.
+    outObj.txs = txDetails
+
+    return outObj
   }
 }
 
--- test/integration/integration.test.js
+++ test/integration/integration.test.js
@@ -11,42 +11,53 @@ const BB2Insight = require('../../index')
 let bb2Insight
 
 const BCHJS = require('@chris.troutner/bch-js')
-//const bchjs = new BCHJS({restURL: `http://192.168.0.36:12400/v3/`})
+// const bchjs = new BCHJS({restURL: `http://192.168.0.36:12400/v3/`})
 const bchjs = new BCHJS({restURL: `http://localhost:3000/v3/`})
 //const bchjs = new BCHJS({restURL: `http://decatur.hopto.org:12400/v3/`})
 
 // Open wallet file for comparing data.
 const wallet = require('../wallet.json')
+//console.log(`wallet: ${JSON.stringify(wallet,null,2)}`)
+
+const insightMocks = require('../mocks/insight-mocks')
 
 describe('#bb2insight', () => {
   before(() => {
     bb2Insight = new BB2Insight()
   })
 
-  it('#address/details should match', async () => {
+  // it('#address/details should match', async () => {
+  //
+  //   // Get data from the Insight API.
+  //   const insightData = await bchjs.Insight.Address.details(wallet.cashAddress)
+  //   console.log(`insightData: ${JSON.stringify(insightData,null,2)}`)
+  //
+  //   // Get converted data from Blockbook API.
+  //   const bbData = await bb2Insight.details(wallet.cashAddress)
+  //   console.log(`Converted Blockbook data: ${JSON.stringify(bbData,null,2)}`)
+  //
+  //   assert.deepEqual(insightData, bbData)
+  // })
 
-    // Get data from the Insight API.
-    const insightData = await bchjs.Insight.Address.details(wallet.cashAddress)
-    //console.log(`insightData: ${JSON.stringify(insightData,null,2)}`)
+  // it('#address/utxo should match', async () => {
+  //
+  //   // Get data from the Insight API.
+  //   const insightData = await bchjs.Insight.Address.utxo(wallet.cashAddress)
+  //   //delete insightData.scriptPubKey
+  //   console.log(`insightData: ${JSON.stringify(insightData,null,2)}`)
+  //
+  //   // Get converted data from Blockbook API.
+  //   const bbData = await bb2Insight.utxo(wallet.cashAddress)
+  //   //console.log(`Converted Blockbook data: ${JSON.stringify(bbData,null,2)}`)
+  //
+  //   assert.deepEqual(insightData, bbData)
+  // })
 
-    // Get converted data from Blockbook API.
-    const bbData = await bb2Insight.details(wallet.cashAddress)
-    //console.log(`Converted Blockbook data: ${JSON.stringify(bbData,null,2)}`)
+  it('#address/transactions should match', async () => {
+    const testAddr = 'bitcoincash:qzrsam87gfd8x5ppgf44hzff4jqp8r706gux4pn79z'
+    //console.log(`insightMocks.txDetails: ${JSON.stringify(insightMocks.txDetails,null,2)}`)
 
-    assert.deepEqual(insightData, bbData)
-  })
-
-  it('#address/utxo should match', async () => {
-
-    // Get data from the Insight API.
-    const insightData = await bchjs.Insight.Address.utxo(wallet.cashAddress)
-    delete insightData.scriptPubKey
-    //console.log(`insightData: ${JSON.stringify(insightData,null,2)}`)
-
-    // Get converted data from Blockbook API.
-    const bbData = await bb2Insight.utxo(wallet.cashAddress)
-    //console.log(`Converted Blockbook data: ${JSON.stringify(bbData,null,2)}`)
-
-    assert.deepEqual(insightData, bbData)
+    const result = await bb2Insight.transactions(testAddr)
+    //console.log(`result: ${JSON.stringify(result,null,2)}`)
   })
 })
